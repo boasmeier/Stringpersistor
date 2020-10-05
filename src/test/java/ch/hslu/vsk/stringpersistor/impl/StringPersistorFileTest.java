367f443b9ad487fa230b403740f60ddf0d7d0940
boasmeier <62314872+boasmeier@users.noreply.github.com>
2020-10-05 09:27:10 +0200

    new: added StringPersistorFileTest

---
+++ src/test/java/ch/hslu/vsk/stringpersistor/impl/StringPersistorFileTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.vsk.stringpersistor.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author boasm
+ */
+public class StringPersistorFileTest {
+
+    public StringPersistorFileTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setFile method, of class StringPersistorFile.
+     */
+    @Test
+    public void testSetFile() {
+
+    }
+
+    /**
+     * Test of save method, of class StringPersistorFile.
+     */
+    @Test
+    public void testSave() {
+
+    }
+
+    /**
+     * Test of get method, of class StringPersistorFile.
+     */
+    @Test
+    public void testGet() {
+
+    }
+
+}
